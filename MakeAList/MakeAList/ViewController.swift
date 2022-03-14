80c514783f6390ae61c96d792828004193665a95
oManhattan <matheusbatuque@gmail.com>
2022-03-14 16:46:13 -0300

    adicionando a base do app

---
+++ MakeAList/MakeAList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MakeAList
+//
+//  Created by Matheus Cavalcanti de Arruda on 14/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
